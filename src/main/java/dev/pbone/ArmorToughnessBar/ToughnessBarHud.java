4e3a05cde3cccb1951bea62ba9a9cbac99a906f6
pbone64 <47093312+pbone64@users.noreply.github.com>
2024-09-30 13:46:16 -0500

    cleanup

--- src/main/java/dev/pbone/ArmorToughnessBar/ToughnessBarHud.java
+++ src/main/java/dev/pbone/ArmorToughnessBar/ToughnessBarHud.java
@@ -1,12 +1,9 @@
 package dev.pbone.ArmorToughnessBar;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.fabricmc.api.ClientModInitializer;
 
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.tag.FluidTags;
